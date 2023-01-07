a92eefa3f0797bb265d0c133d40fb8f5955c8fc5
RedRastaRigger <87661155+RedRastaRigger@users.noreply.github.com>
2023-01-07 02:22:33 +0100

    workspace settings added

--- Src/ECS/Component.gd
+++ Src/ECS/Component.gd
@@ -1,16 +1,13 @@
-extends Node
+extends Object
+class_name Component
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# VAR > Rarely needed, helpfull for debugging    
+var owner
+
+# VAR > Needs to be named after class of child!  
+# VAR > used as Key in [Entity.components]       
+var name
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- Src/ECS/Entity.gd
+++ Src/ECS/Entity.gd
@@ -1,16 +1,103 @@
 extends Node
+class_name Entity
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# ----- INITIALICE --------------------------------------------------------- ##
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# VAR > INDEX_NAME : INSTANCE     
+var templates  : Dictionary = {}
+var components : Dictionary = {}
+
+# VAR > ENUMs
+var flags : Array = []
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+# ----- GETTER ------------------------------------------------------------- ##
+
+
+#>>> Components <<<#
+
+# FUNC > returns instance! use [getCompValue] for its content  
+func getComp(compName:String) -> Component:	
+	return self.components[compName]
+
+# FUNC > Check [compInst.getType_quack()] for type of return 
+func getCompValue(compName:String): 
+	return self.components[compName].value
+
+func hasComp(componentName:String) -> bool:
+	return self.components.has(componentName)
+
+
+#>>> Templates <<<#
+
+# FUNC > Flyweight-pattern, Instances are shared globally! 
+func getCompTemp(tempName:String, compName:String) -> Component:  
+	return templates[tempName].components[compName]
+
+# FUNC > Flyweight-pattern, Instances are shared globally! 
+# FUNC > Check [compInst.getType_quack()] for type of return 
+func getCompTempValue(tempName:String, compName:String):
+	return getCompTemp(tempName,compName).value
+
+func hasTemplate(tempName:String) -> bool:
+	return self.templates.has(tempName)
+
+func getFlagTemp(tempName:String):
+	return templates[tempName].flags
+
+
+
+func hasFlag(flagName:String)  -> bool:   return flags.has(flagName)
+
+
+
+# ----- SETTER ------------------------------------------------------------- ##
+
+
+#>>> Components <<<#
+
+
+# FUNC > Has to be indexed children of Component                
+func addComponent(compInstance:Component) -> void:
+	components[compInstance.name] = compInstance
+	compInstance.owner = self
+
+# FUNC > will not [queue_free()] the Component!                 
+func removeComponent(compName:String) -> void:
+	if hasComp(compName):  components.erase(compName)	
+
+
+#>>> Templates <<<#
+
+
+func addTemplate(templateName:String, ent:Entity) -> void:
+	templates[templateName] = ent
+
+func removeTemplate(tempName:String) -> void:
+	if hasTemplate(tempName):   templates.erase(tempName)
+
+
+#>>> Flags <<<#
+
+
+func addFlag(flagName:String) -> void:
+	flags.append(flagName)
+
+func removeFlag(flagName:String) -> void:
+	flags.erase(flagName)
+
+
+#>>> Properties <<<#
+
+
+func addProperty(propEnum:int) -> void:
+	properties.append(propEnum)
+
+func removeProperty(propEnum:int) -> void:
+	properties.erase(propEnum)
+
+
+
